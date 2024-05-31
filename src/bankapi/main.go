ba1a4950de2770f6a6953413dd37814bcad8ac0b
dinglierice <dinglie1997@163.com>
2024-05-31 22:59:01 +0800

    fix

--- src/bankapi/main.go
+++ src/bankapi/main.go
@@ -21,6 +21,8 @@ func main() {
 	}
 
 	http.HandleFunc("/statement", statement)
+	http.HandleFunc("/deposit", deposit)
+	http.HandleFunc("/withdraw", withdraw)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
@@ -40,6 +42,60 @@ func statement(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(w, "Account with number %v can't be found!", number)
 		} else {
 			fmt.Fprintf(w, account.Statement())
+		}
+	}
+}
+
+func deposit(w http.ResponseWriter, req *http.Request) {
+	numberqstr := req.URL.Query().Get("number")
+	amountstr := req.URL.Query().Get("amount")
+
+	if numberqstr == "" {
+		fmt.Fprintf(w, "Account number and amount is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqstr, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountstr, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account amount!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with amount %v can't be found!", amount)
+		} else {
+			err := account.Deposit(amount)
+			if err != nil {
+				fmt.Fprintf(w, "Account with amount %v deposit failed!", amount)
+			} else {
+				fmt.Fprintf(w, "Account with amount %v successfully deposited!", amount)
+			}
+		}
+	}
+}
+
+func withdraw(w http.ResponseWriter, req *http.Request) {
+	numberqstr := req.URL.Query().Get("number")
+	amountstr := req.URL.Query().Get("amount")
+
+	if numberqstr == "" {
+		fmt.Fprintf(w, "Account number and amount is missing!")
+	}
+
+	if number, err := strconv.ParseFloat(numberqstr, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountstr, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account amount!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		}
+		err := account.Withdraw(amount)
+		if err != nil {
+			fmt.Fprintf(w, "Account with amount %v deposit failed!", amount)
+		} else {
+			fmt.Fprintf(w, "Account with amount %v successfully withdrawn!", amount)
 		}
 	}
 }
